252c7f5b7b32187097ff21fd910dbbdbccb96a44
NotEvil <notevil036@gmail.com>
2026-03-01 01:02:17 +0200

    Delete old test sandbox code

--- backend/src/main/java/org/kindness/backend/BackendApplication.java
+++ backend/src/main/java/org/kindness/backend/BackendApplication.java
@@ -1,7 +1,7 @@
 package org.kindness.backend;
 
 import org.kindness.api.dao.BaseDao;
-import org.kindness.api.model.Author;
+import org.kindness.api.model.impl.User;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,28 +17,9 @@ public class BackendApplication {
     }
 
     @Bean
-    public ApplicationRunner dataAccessCheck(BaseDao<Author> dao){
+    public ApplicationRunner dataAccessCheck(BaseDao<User> dao){
         return args -> {
-            Author author = new Author("deva", "be", LocalDate.MAX);
-            Author author1 = new Author("dev", "ba", LocalDate.MAX);
-            Author author2 = new Author("de", "bc", LocalDate.MAX);
 
-            dao.insert(author);
-            dao.insert(author1);
-            dao.insert(author2);
-
-            var authors = dao.findAll();
-            authors.forEach(s -> {
-                System.out.println(s.getFirstName());
-            });
-
-            dao.deleteById(1L);
-            System.out.println(dao.findById(1L));
-
-            var authors1 = dao.findAll();
-            authors1.forEach(s -> {
-                System.out.println(s.getFirstName());
-            });
         };
     }
 }
