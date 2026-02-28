3b0f53b0ebf051a4055d59f396264da956b4a756
NotEvil <notevil036@gmail.com>
2026-03-01 01:17:07 +0200

    Add first test

--- backend/src/test/java/org/kindness/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/kindness/backend/BackendApplicationTests.java
@@ -1,13 +1,35 @@
 package org.kindness.backend;
 
 import org.junit.jupiter.api.Test;
+import org.kindness.api.dao.BaseDao;
+import org.kindness.api.model.impl.Zone;
+import org.kindness.backend.dao.JdbcZoneDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @Autowired
+    private JdbcZoneDao zoneDao;
+
     @Test
     void contextLoads() {
+        Zone zone = new Zone("Mingi nimi", 52);
+
+        zoneDao.insert(zone);
+
+        List<Zone> allZones = zoneDao.findAll();
+        Zone savedZone = allZones.getLast();
+
+        assertNotNull("Id should be presented", savedZone.getZoneId());
+        assertEquals("Names should be same", zone.getName(), savedZone.getName());
+        assertEquals("Capacity should be same", zone.getCapacity(), savedZone.getCapacity());
     }
 
 }
