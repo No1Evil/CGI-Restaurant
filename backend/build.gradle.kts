9d93737fc1b58f23fb397dfeb35d84cf24867002
NotEvil <notevil036@gmail.com>
2026-02-28 22:29:16 +0200

    Add implementations of api module and h2

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,12 +22,12 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":api"))
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jdbc-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
