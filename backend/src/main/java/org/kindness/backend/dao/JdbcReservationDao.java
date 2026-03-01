c9824fd9d09e3ce0493cfdb701149cb62fd01bf3
NotEvil <notevil036@gmail.com>
2026-03-01 17:36:43 +0200

    Move complex queries into scripts folder

--- backend/src/main/java/org/kindness/backend/dao/JdbcReservationDao.java
+++ backend/src/main/java/org/kindness/backend/dao/JdbcReservationDao.java
@@ -1,12 +1,17 @@
 package org.kindness.backend.dao;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.kindness.api.dao.BaseDao;
 import org.kindness.api.model.impl.Reservation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +19,14 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public final class JdbcReservationDao implements BaseDao<Reservation> {
     private final JdbcTemplate jdbcTemplate;
+
+    @Value("classpath:sql/scripts/is_time_taken.sql")
+    private Resource isTimeTakenScript;
+
+    @PostConstruct
+    public void init() throws IOException {
+        IS_TIME_TAKEN = new String(isTimeTakenScript.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+    }
 
     public final RowMapper<Reservation> mapper = (rs, rowNum) -> new Reservation(
             rs.getLong("id"),
@@ -33,10 +46,7 @@ public final class JdbcReservationDao implements BaseDao<Reservation> {
     private static final String REMOVE_QUERY = "UPDATE \"tables\" SET is_deleted = TRUE WHERE id = ?";
     private final static String FIND_ALL_QUERY = "SELECT * FROM \"reservations\"";
     private final static String FIND_BY_ID_QUERY = "SELECT * FROM \"reservations\" WHERE id=?";
-    private final static String IS_TIME_TAKEN =
-            "SELECT COUNT(*) FROM \"reservations\" " +
-            "WHERE table_id = ? " +
-            "AND (reservation_start < ? and reservation_end > ?)";
+    private static String IS_TIME_TAKEN;
 
     @Override
     public void insert(Reservation model) {
@@ -58,6 +68,10 @@ public final class JdbcReservationDao implements BaseDao<Reservation> {
     @Override
     public Optional<Reservation> findById(Long id) {
         return jdbcTemplate.query(FIND_BY_ID_QUERY, mapper).stream().findFirst();
+    }
+
+    public boolean isTimeTaken(Reservation res){
+        return this.isTimeTaken(res.getTableId(), res.getReservationStart(), res.getReservationEnd());
     }
 
     public boolean isTimeTaken(long table_id, LocalDateTime start, LocalDateTime end){
--- backend/src/main/java/org/kindness/backend/dao/JdbcTableDao.java
+++ backend/src/main/java/org/kindness/backend/dao/JdbcTableDao.java
@@ -1,12 +1,19 @@
 package org.kindness.backend.dao;
 
+import jakarta.annotation.Nullable;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.kindness.api.dao.BaseDao;
 import org.kindness.api.model.impl.Table;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +21,15 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public final class JdbcTableDao implements BaseDao<Table> {
     private final JdbcTemplate jdbcTemplate;
+
+    // https://www.baeldung.com/spring-classpath-file-access
+    @Value("classpath:sql/scripts/find_available_tables.sql")
+    private Resource findAvailableTablesScript;
+
+    @PostConstruct
+    public void init() throws IOException {
+        FIND_AVAILABLE_TABLES_QUERY = new String(findAvailableTablesScript.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+    }
 
     private static final RowMapper<Table> mapper = (rs, rowNum) -> new Table(
             rs.getLong("id"),
@@ -30,11 +46,8 @@ public final class JdbcTableDao implements BaseDao<Table> {
     private static final String REMOVE_QUERY = "UPDATE \"tables\" SET is_deleted = TRUE WHERE id = ?";;
     private final static String FIND_ALL_QUERY = "SELECT * FROM \"tables\"";
     private final static String FIND_BY_ID_QUERY = "SELECT * FROM \"tables\" WHERE id=?";
-    private final static String FIND_ALL_BY_ZONE_ID = "SELECT * from \"tables\" WHERE zone_id=?";
-    private final static String COUNT_TABLES_IN_THE_ZONE =
-                    "select t.*" +
-                    "from \"zone\" z" +
-                    "join \"table\" t on z.zone_id = t.zone_id";
+    private final static String FIND_ALL_BY_ZONE_ID_QUERY = "SELECT * from \"tables\" WHERE zone_id=?";
+    private static String FIND_AVAILABLE_TABLES_QUERY;
 
     @Override
     public void insert(Table model) {
@@ -58,7 +71,16 @@ public final class JdbcTableDao implements BaseDao<Table> {
                 .findFirst();
     }
 
-    public List<Table> findAllByZoneId(Long zoneId){
-        return jdbcTemplate.query(FIND_ALL_BY_ZONE_ID, mapper, zoneId);
+    /**
+     * @return all available tables within startTime and endTime
+     */
+    public List<Table> findAllAvailable(@Nullable Long zoneId, @Nullable Integer capacity, Timestamp startTime, Timestamp endTime) {
+        return jdbcTemplate.query(FIND_AVAILABLE_TABLES_QUERY, mapper,
+                zoneId, zoneId, capacity, capacity,
+                endTime, startTime);
+    }
+
+    public List<Table> findAllByZoneId(Long zoneId) {
+        return jdbcTemplate.query(FIND_ALL_BY_ZONE_ID_QUERY, mapper, zoneId);
     }
 }
