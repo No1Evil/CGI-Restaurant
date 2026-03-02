414144c950f6b17a09e8144eea82dd4422baa1c1
NotEvil <notevil036@gmail.com>
2026-03-02 13:45:13 +0200

    Prepare protobuf

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,4 +1,10 @@
 dependencies {
+
+    implementation("com.google.protobuf:protobuf-java:4.34.0")
+    implementation("io.grpc:grpc-netty-shaded:1.79.0")
+    implementation("io.grpc:grpc-stub:1.79.0")
+    implementation("io.grpc:grpc-protobuf:1.79.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
