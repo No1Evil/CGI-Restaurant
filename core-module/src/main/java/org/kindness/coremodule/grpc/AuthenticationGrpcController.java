03f784f8e55501fa613b3c049a3ff60e34d0282c
NotEvil <notevil036@gmail.com>
2026-03-07 15:37:41 +0200

    Return jwt token on login

--- core-module/src/main/java/org/kindness/coremodule/grpc/AuthenticationGrpcController.java
+++ core-module/src/main/java/org/kindness/coremodule/grpc/AuthenticationGrpcController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.kindness.common.grpc.user.AuthenticationServiceGrpc;
 import org.kindness.common.grpc.user.LoginValidationResponse;
 import org.kindness.common.grpc.user.ValidateLoginRequest;
+import org.kindness.common.model.impl.User;
+import org.kindness.coremodule.domain.security.JwtTokenProvider;
 import org.kindness.coremodule.domain.user.AuthenticationService;
 import org.kindness.coremodule.util.GrpcServiceUtil;
 import org.springframework.grpc.server.service.GrpcService;
@@ -13,12 +15,18 @@ import org.springframework.grpc.server.service.GrpcService;
 @RequiredArgsConstructor
 public final class AuthenticationGrpcController extends AuthenticationServiceGrpc.AuthenticationServiceImplBase {
     private final AuthenticationService authenticationService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public void login(ValidateLoginRequest request, StreamObserver<LoginValidationResponse> observer) {
         var responseBuilder = LoginValidationResponse.newBuilder();
         GrpcServiceUtil.handleRequest(responseBuilder, observer, () -> {
-            authenticationService.login(request.getEmail(), request.getPassword());
+            User user = authenticationService.login(request.getEmail(), request.getPassword());
+            if (user == null) throw new IllegalStateException("Something went wrong");
+
+            String token = jwtTokenProvider.createToken(user.getUserId(), user.getEmail());
+            responseBuilder.setToken(token);
+            responseBuilder.setUserId(user.getUserId());
         });
     }
 }
