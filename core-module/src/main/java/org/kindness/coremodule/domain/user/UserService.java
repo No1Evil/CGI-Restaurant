887e463e5a0d01c049eac9c5ed41ea5018553446
NotEvil <notevil036@gmail.com>
2026-03-07 14:03:11 +0200

    Implement UserService.java

--- core-module/src/main/java/org/kindness/coremodule/domain/user/UserService.java
+++ core-module/src/main/java/org/kindness/coremodule/domain/user/UserService.java
@@ -1,7 +1,25 @@
 package org.kindness.coremodule.domain.user;
 
+import lombok.RequiredArgsConstructor;
+import org.kindness.common.model.impl.User;
+import org.kindness.module.persistence.dao.impl.JdbcUserDao;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
+    private final JdbcUserDao userDao;
+
+    public User getUserData(Long id){
+        if (id == null) throw new IllegalStateException("Id cannot be null");
+        return userDao.findById(id).orElseThrow(() -> new IllegalStateException("No such user"));
+    }
+
+    public List<User> getAllUsers(){
+        return userDao.findAll();
+    }
 }
