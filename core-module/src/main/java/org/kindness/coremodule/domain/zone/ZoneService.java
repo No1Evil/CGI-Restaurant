167c3597cef0bb83c2e2e83a63be676133a55db5
NotEvil <notevil036@gmail.com>
2026-03-07 15:13:53 +0200

    Implement ZoneService

--- core-module/src/main/java/org/kindness/coremodule/domain/zone/ZoneService.java
+++ core-module/src/main/java/org/kindness/coremodule/domain/zone/ZoneService.java
@@ -1,7 +1,26 @@
 package org.kindness.coremodule.domain.zone;
 
+import lombok.RequiredArgsConstructor;
+import org.kindness.common.model.impl.Zone;
+import org.kindness.module.persistence.dao.impl.JdbcZoneDao;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
-public final class ZoneService {
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ZoneService {
+    private final JdbcZoneDao zoneDao;
+
+    public Zone getZone(Long id){
+        if (id == null) throw new IllegalStateException("Zone id cannot be null");
+        return zoneDao.findById(id)
+                .orElseThrow(() -> new IllegalStateException("No zone found"));
+    }
+
+    public List<Zone> getAllZones(){
+        return zoneDao.findAll();
+    }
 }
