1ce89b513e4d69ee0badd678daea7a4a93646fdf
NotEvil <notevil036@gmail.com>
2026-03-07 11:09:08 +0200

    Implement RestaurantGrpcController

--- core-module/src/main/java/org/kindness/coremodule/grpc/RestaurantGrpcController.java
+++ core-module/src/main/java/org/kindness/coremodule/grpc/RestaurantGrpcController.java
@@ -2,17 +2,53 @@ package org.kindness.coremodule.grpc;
 
 import io.grpc.stub.StreamObserver;
 import org.kindness.common.grpc.restaraunt.*;
+import org.kindness.common.model.impl.Restaurant;
+import org.kindness.coremodule.domain.restaurant.RestaurantService;
+import org.kindness.coremodule.util.TimestampConverter;
 import org.springframework.grpc.server.service.GrpcService;
+
+import java.util.List;
 
 @GrpcService
 public final class RestaurantGrpcController extends RestaurantServiceGrpc.RestaurantServiceImplBase {
+    private RestaurantService restaurantService;
+
     @Override
-    public void getAllRestaurantsData(AllRestaurantsDataRequest request, StreamObserver<AllRestaurantsDataResponse> responseObserver) {
-        super.getAllRestaurantsData(request, responseObserver);
+    public void getAllRestaurantsData(AllRestaurantsDataRequest request, StreamObserver<AllRestaurantsDataResponse> observer) {
+        List<Restaurant> allRestaurants = restaurantService.getAllRestaurants();
+        var response = AllRestaurantsDataResponse.newBuilder()
+                .addAllData(toResponse(allRestaurants))
+                .build();
+
+        observer.onNext(response);
+        observer.onCompleted();
     }
 
     @Override
-    public void getRestaurantData(RestaurantDataRequest request, StreamObserver<RestaurantDataResponse> responseObserver) {
-        super.getRestaurantData(request, responseObserver);
+    public void getRestaurantData(RestaurantDataRequest request, StreamObserver<RestaurantDataResponse> observer) {
+        long restaurantId = request.getRestaurantId();
+        Restaurant restaurant = restaurantService.getRestaurant(restaurantId);
+        if (restaurant == null) {
+            observer.onCompleted();
+            return;
+        }
+        RestaurantDataResponse response = toResponse(restaurant);
+        observer.onNext(response);
+        observer.onCompleted();
+    }
+
+    private static List<RestaurantDataResponse> toResponse(List<Restaurant> restaurants){
+        return restaurants.stream().map(RestaurantGrpcController::toResponse).toList();
+    }
+
+    private static RestaurantDataResponse toResponse(Restaurant restaurant){
+        return RestaurantDataResponse.newBuilder()
+                .setName(restaurant.getName())
+                .setAddress(restaurant.getAddress())
+                .setPhone(restaurant.getPhone())
+                .setEmail(restaurant.getEmail())
+                .setOpenTime(TimestampConverter.fromLocalTime(restaurant.getOpenTime()))
+                .setCloseTime(TimestampConverter.fromLocalTime(restaurant.getCloseTime()))
+                .build();
     }
 }
