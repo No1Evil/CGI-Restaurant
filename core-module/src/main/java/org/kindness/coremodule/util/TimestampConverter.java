2692d3e59eb638490e97faa09419a45b11afb342
NotEvil <notevil036@gmail.com>
2026-03-07 10:44:36 +0200

    Add util to convert protobuf Timestamp to LocalDate time

---
+++ core-module/src/main/java/org/kindness/coremodule/util/TimestampConverter.java
@@ -0,0 +1,17 @@
+package org.kindness.coremodule.util;
+
+import com.google.protobuf.Timestamp;
+
+import java.time.*;
+
+public class TimestampConverter {
+    // Source - https://stackoverflow.com/a/52651480
+    // Posted by Basil Bourque, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-03-03, License - CC BY-SA 4.0
+    public static LocalDateTime convert(Timestamp ts){
+        Instant instant =  Instant.ofEpochSecond( ts.getSeconds() , ts.getNanos() ) ;
+        ZoneId z = ZoneId.of("UTC");
+        ZonedDateTime zdt = instant.atZone( z ) ;
+        return zdt.toLocalDateTime();
+    }
+}
