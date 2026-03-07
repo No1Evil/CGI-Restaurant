c2280915a3b1885d1894c114ee57d8bb14f8c946
NotEvil <notevil036@gmail.com>
2026-03-07 14:02:57 +0200

    Code-cleanup for marking methods read-only transactions

--- core-module/src/main/java/org/kindness/coremodule/domain/restaurant/RestaurantService.java
+++ core-module/src/main/java/org/kindness/coremodule/domain/restaurant/RestaurantService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class RestaurantService {
     private JdbcRestaurantDao restaurantDao;
 
