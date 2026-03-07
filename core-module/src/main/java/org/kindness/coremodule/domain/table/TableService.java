883e98ff851a559ebac3234e09c97f42513d326b
NotEvil <notevil036@gmail.com>
2026-03-07 15:09:28 +0200

    Implement TableService

--- core-module/src/main/java/org/kindness/coremodule/domain/table/TableService.java
+++ core-module/src/main/java/org/kindness/coremodule/domain/table/TableService.java
@@ -1,7 +1,27 @@
 package org.kindness.coremodule.domain.table;
 
+import lombok.RequiredArgsConstructor;
+import org.kindness.common.model.impl.Table;
+import org.kindness.module.persistence.dao.impl.JdbcTableDao;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TableService {
+    private final JdbcTableDao tableDao;
+
+    public Table getTableData(Long tableId){
+        if (tableId == null) throw new IllegalStateException("Table id cannot be null");
+        return tableDao.findById(tableId)
+                .orElseThrow(() -> new IllegalStateException("No table found"));
+    }
+
+    public List<Table> getAllTablesData(){
+        return tableDao.findAll();
+    }
 }
