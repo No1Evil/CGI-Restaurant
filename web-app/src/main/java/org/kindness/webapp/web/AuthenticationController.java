5bc3cde663fd5eb1c50f18a57353564ca0d9d23a
NotEvil <notevil036@gmail.com>
2026-03-10 21:25:19 +0200

    Split AuthenticationController to RegistrationController

--- web-app/src/main/java/org/kindness/webapp/web/AuthenticationController.java
+++ web-app/src/main/java/org/kindness/webapp/web/AuthenticationController.java
@@ -3,6 +3,7 @@ package org.kindness.webapp.web;
 import lombok.RequiredArgsConstructor;
 import org.kindness.common.grpc.user.*;
 import org.kindness.common.model.impl.User;
+import org.kindness.webapp.model.AuthResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +18,7 @@ import java.security.Principal;
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 public class AuthenticationController {
     private final AuthenticationServiceGrpc.AuthenticationServiceBlockingStub authService;
-    private final RegistrationServiceGrpc.RegistrationServiceBlockingStub registrationService;
     private final UserServiceGrpc.UserServiceBlockingStub userService;
-
-    public record AuthResponse(User user, String token){}
 
     @PostMapping("/auth/login")
     public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password) {
@@ -54,47 +52,6 @@ public class AuthenticationController {
                 .globalRole(data.getRole()).build();
 
         return ResponseEntity.ok(new AuthResponse(user, response.getToken()));
-    }
-
-    @PostMapping("/auth/register")
-    public ResponseEntity<?> register(@RequestParam String email,
-                         @RequestParam String password,
-                         @RequestParam String firstName,
-                         @RequestParam String secondName) {
-
-        RegisterRequest request = RegisterRequest.newBuilder()
-                .setEmail(email)
-                .setPassword(password)
-                .setFirstName(firstName)
-                .setSecondName(secondName)
-                .build();
-
-        RegisterResponse registerResponse = registrationService.register(request);
-
-        if (!registerResponse.getSuccess()){
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(registerResponse.getMessage());
-        }
-
-        var userResponse = userService.getUser(UserRequest.newBuilder()
-                .setUserId(registerResponse.getUserId()).build());
-
-        if (!userResponse.getSuccess()){
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(userResponse.getMessage());
-        }
-
-        var data = userResponse.getData();
-
-        var user = User.builder()
-                .userId(data.getUserId())
-                .firstName(data.getFirstName())
-                .secondName(data.getSecondName())
-                .email(data.getEmail())
-                .globalRole(data.getRole())
-                .build();
-
-        return ResponseEntity.ok(new AuthResponse(user, registerResponse.getToken()));
     }
 
     @GetMapping("/auth/me")
