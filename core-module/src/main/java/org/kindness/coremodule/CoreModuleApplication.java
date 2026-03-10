d7d9fa527203af6c171eb13c1e6ee7f3817da7a9
NotEvil <notevil036@gmail.com>
2026-03-10 21:13:14 +0200

    Scan more packages in core-app

--- core-module/src/main/java/org/kindness/coremodule/CoreModuleApplication.java
+++ core-module/src/main/java/org/kindness/coremodule/CoreModuleApplication.java
@@ -2,8 +2,12 @@ package org.kindness.coremodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "org.kindness.module.persistence.dao")
+@SpringBootApplication(scanBasePackages = {
+        "org.kindness.module.persistence.dao",
+        "org.kindness.coremodule"
+})
 public class CoreModuleApplication {
 
     public static void main(String[] args) {
